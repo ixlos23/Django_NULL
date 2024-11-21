ded24e2a7949976377c7e72955d50e44e780c626
ixlos23 <izzatmatkarimov090@gmail.com>
2024-11-21 17:18:02 +0500

    change 3

--- root/asgi.py
+++ root/asgi.py
@@ -9,14 +9,19 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 
 import os
 
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
+
+from apps.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'root.settings')
 
 application = ProtocolTypeRouter(
     {
         "http":  get_asgi_application(),
-        "websocket": AllowedHostsOriginalValidator(
+        "websocket": AllowedHostsOriginValidator(
             AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
         ),
     }
